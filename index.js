c0aff1a9f859c5b773e3dc4b53ead24202224806
EmreSahiner <emre@journeyapp.com>
2023-03-23 10:44:08 +0300

    first version

---
+++ index.js
@@ -0,0 +1,19 @@
+//console.log(process.argv);
+/* const foo = process.argv[2];
+const user = process.env.USER;
+console.log(foo);
+console.log(user);
+ */
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+const port = process.env.PORT || 3000;
+
+server.get('/hello', (req,res)=> {
+    res.status(200).json(process.env.MESSAGE || 'Merhaba...');
+})
+
+server.listen(port, ()=> {
+    console.log(`server listening on port ${port}`);
+})
